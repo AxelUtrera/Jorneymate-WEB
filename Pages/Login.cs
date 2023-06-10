a1cb03617c4576e735fa3abd7f2be84c1b8d910f
Axel Utrera <axeljordanomu@gmail.com>
2023-06-09 23:36:36 -0600

    Creacion de pantalla de login

--- Pages/Login.cs
+++ Pages/Login.cs
@@ -1,8 +1,33 @@
+using Jorneymate_WEB.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Jorneymate_WEB.Pages;
-
-public class Login : PageModel
+namespace Jorneymate_WEB.Pages
 {
-    
+    public class Login : PageModel
+    {
+        public User UserLogged { get; set; }
+
+        [BindProperty]
+        public string Email { get; set; } 
+
+        [BindProperty]
+        public string Password { get; set; }
+        
+        public async Task<IActionResult> OnPostAsync()
+        {
+            User userResult = await Logic.Autentication.Login(Email, Password);
+
+            if (userResult != null)
+            {
+                UserLogged = userResult;
+                return RedirectToPage("/Index");
+            }
+            else
+            {
+                TempData["Error"] = "Los datos ingresados son incorrectos. Intenta nuevamente.";
+                return Page();
+            }
+        }
+    }
 }
